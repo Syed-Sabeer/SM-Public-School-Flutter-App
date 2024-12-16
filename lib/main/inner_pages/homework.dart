1fe64739e70fcc27d1aefcc44d52e55a93de857a
Syed-Sabeer <syedsabeer.techie@gmail.com>
2024-12-16 22:51:35 +0500

    working on homework

--- lib/main/inner_pages/homework.dart
+++ lib/main/inner_pages/homework.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import '../widgets/app_bar.dart';
 
 class HomeworkView extends StatelessWidget {
   const HomeworkView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Homework View")));
+    return Scaffold(
+      appBar: const CustomAppBar(
+        title: 'Homework',
+        isDashboard: false,
+      ),
+      backgroundColor: Colors.white,
+      );
   }
+
+
+  
 }
